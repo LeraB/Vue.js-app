c1226cdb8246ce9556e14020e5992179465227c9
valeriia.briantseva <kudravka13@gmail.com>
2020-01-06 17:12:21 +0200

    Fix class names

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,25 +1,25 @@
 <template>
     <div class="hello">
         <h1>Weather in {{ currentCity }} </h1>
-        <form @submit.prevent="getData" action="/">
-            <input v-model="city" type="text">
-            <select v-model="country">
+        <form @submit.prevent="getData" action="/" class="">
+            <input class="field-city" v-model="city" type="text">
+            <select class="field-country" v-model="country">
                 <option value="ua">UA</option>
                 <option value="usa">USA</option>
             </select>
-            <button type="submit">Submit</button>
+            <button class="btn" type="submit">Submit</button>
         </form>
         <template v-if="list">
-            <div class="weather-list" v-for="(list, index) in list" :key="index">
-                <h2> {{ getDate(list) }} </h2>
-                <ul v-if="getTime(list)==='00:00:00'">
-                    <li>
-                        <h3>{{ getTime(list) }}</h3>
+            <div class="weather-list">
+                <ul class="list">
+                    <li class="list__item" v-for="(list, index) in list" :key="index">
+                        <h2 class="title--secondary"> {{ getDate(list) }} </h2>
+                        <h3 class="title--third-level">{{ getTime(list) }}</h3>
                         <div>
                             <p> Min temperature: {{ list.main.temp_min }}</p>
                             <p> Max temperature: {{ list.main.temp_max }}</p>
                             <figure>
-                                <img v-bind:src="'http://openweathermap.org/img/wn/' + list.weather[0].icon + '.png'">
+                                <img class="list__icon" v-bind:src="'http://openweathermap.org/img/wn/' + list.weather[0].icon + '.png'">
                                 <figcaption>{{ list.weather[0].description}}</figcaption>
                             </figure>
                         </div>
@@ -66,21 +66,29 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    h2 {
+    .title--secondary {
+        font-weight: bold;
+        margin-top: 0;
+        margin-bottom: 10px;
+    }
+
+    .title--third-level {
         font-weight: bold;
         margin-top: 0;
         margin-bottom: 25px;
+        margin-top: 0px;
     }
 
-    li {
+    .list__item {
         display: inline-block;
         padding: 20px;
         margin: 20px;
         border-radius: 10px;
         box-shadow: 0 10px 16px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+        width: 200px;
     }
 
-    input {
+    .field-city {
         padding: 10px;
         border-radius: 10px;
         border: 1px solid lightgrey;
@@ -88,7 +96,7 @@
         margin-right: 10px;
     }
 
-    select {
+    .field-country {
         padding: 10px;
         border-radius: 10px;
         border: 1px solid lightgrey;
@@ -97,7 +105,7 @@
         background-color: white;
     }
 
-    button {
+    .btn {
         padding: 10px;
         border-radius: 10px;
         border: 1px solid lightgrey;
@@ -107,12 +115,12 @@
         cursor: pointer;
     }
 
-    img {
+    .list__icon {
         margin: 0 auto;
     }
 
     @media (min-width: 1200px) {
-        ul {
+        .list {
             max-width: 70%;
             margin: 0 auto;
             margin-top: 50px;
